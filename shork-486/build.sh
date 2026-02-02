6c93899e833a9016e8530dfcfaca2b174af3d564
kali <sharktallica@gmail.com>
2026-02-02 06:02:09 +0000

    Patches for fixing "x.bin: too big (y > z)" issues during get_patched_extlinux

--- shork-486/build.sh
+++ shork-486/build.sh
@@ -778,11 +778,17 @@ get_patched_extlinux()
         echo -e "${YELLOW}EXTLINUX source already present, resetting...${RESET}"
         cd syslinux
         git reset --hard
+        make clean || true
     else
         echo -e "${GREEN}Downloading EXTLINUX...${RESET}"
         git clone https://github.com/SharktasticA/syslinux.git
         cd syslinux
     fi
+
+    # Patches for fixing "x.bin: too big (y > z)" issues
+    sudo sed -i 's/\$maxsize = \$padsize = 440;/\$maxsize = \$padsize = 500;/' mbr/checksize.pl
+    sudo sed -i 's/\$maxsize = \$padsize = 432;/\$maxsize = \$padsize = 500;/' mbr/checksize.pl
+    sudo sed -i 's/\$maxsize = \$padsize = 439;/\$maxsize = \$padsize = 500;/' mbr/checksize.pl
 
     # Compile and install
     echo -e "${GREEN}Compiling EXTLINUX...${RESET}"
